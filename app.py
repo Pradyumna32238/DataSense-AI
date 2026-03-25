ed553fe25642e1cc60c0fddcaff1c12a10a6b422
Yeshwanth G <yeshwanthreddy312@gmail.com>
2026-03-25 22:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ app = create_app()
 if __name__ == '__main__':
     # This allows running the app directly with 'python app.py' for local development.
     # For production, Gunicorn will be used.
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(debug=True)
