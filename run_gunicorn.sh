faa151a2c342c72134abc10c19f8e68aa768f92b
Yeshwanth <142969566+Pradyumna32238@users.noreply.github.com>
2026-03-26 13:37:34 +0530

    Add files via upload

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-gunicorn --bind 0.0.0.0:${PORT:-8000} "app:create_app()" --workers 1 --log-level debug
+#!/bin/sh
+echo "Starting server with Gunicorn..."
+
+# Use --preload to load app before worker fork, but embedding model is lazy-loaded in embedding_service.py
+gunicorn --workers 4 \
+         --bind 0.0.0.0:${PORT:-8000} \
+         --preload \
+         --log-level info \
+         "app:create_app()"
